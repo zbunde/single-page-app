67df928e439abfa1aaf02c8a5687f06f19d43da2
Jeff Dean <jeff@galvanize.it>
2014-06-10 20:16:00 -0600

    users can create people

--- app/assets/javascripts/people_app.js
+++ app/assets/javascripts/people_app.js
@@ -14,6 +14,7 @@ window.PeopleApp = {
 
   initialize: function (path) {
     $.getJSON(path, this.renderInitialPage);
+    $(document).on("submit", "[data-behavior=create-person]", this.didSubmitPersonCreateForm.bind(this));
   },
 
   // This function is called with data from GET /people
@@ -23,10 +24,83 @@ window.PeopleApp = {
   //  * iterating through each person appending the person-show dom to the page
   //
   renderInitialPage: function (response) {
+    var html = JST['templates/person_new']({
+      url: response._links.self.href,
+      first_name: response._embedded.person.first_name,
+      last_name: response._embedded.person.last_name,
+      address: response._embedded.person.address
+    });
+    $("[data-container=main]").append(html);
+
     $.each(response._embedded.people, function () {
       var $html = $(JST['templates/person_show'](this));
       $html.data('person', this);
       $("[data-container=people]").append($html);
+    });
+  },
+
+  // This function is called when a person clicks the submit button on the new person form
+  // It is responsible for:
+  //
+  //  * creating a json string to send to the server, based off of the form fields (first name, last name etc...)
+  //  * posting that json string to the server
+  //  * setting up the callbacks that will be triggered when the server returns a 200 OK
+  //  * setting up the callbacks that will be triggered when the server returns with validation errors
+  //
+  didSubmitPersonCreateForm: function (event) {
+    event.preventDefault();
+
+    var formParams = {};
+    $.each($(event.target).serializeArray(), function (object) {
+      formParams[this.name] = this.value;
+    });
+    var jsonForServer = JSON.stringify(formParams);
+
+    var jqxhr = $.ajax({
+      type: "POST",
+      url: event.target.action,
+      data: jsonForServer,
+      dataType: 'json',
+      contentType: 'application/json'
+    });
+
+    jqxhr.done(function (object) {
+      this.personWasCreated(event.target, object);
+    }.bind(this));
+
+    jqxhr.fail(function (xhr) {
+      this.personWasNotSaved(event.target, xhr.responseJSON);
+    }.bind(this));
+  },
+
+  // This function is called when the server returns a 200 after creating a person
+  // It is responsible for:
+  //
+  //  * resetting the "new" form (removing error messages, clearing the fields etc...)
+  //  * appending a new person to the list of people
+  //
+  personWasCreated: function (form, person) {
+    $("[data-container=errors]").empty();
+    $(".field-with-errors").removeClass("field-with-errors");
+    form.reset();
+    var $html = $(JST['templates/person_show'](person));
+    $html.data('person', person);
+    $("[data-container=people]").append($html);
+  },
+
+  // This function is called when the server returns with validation errors after attempting to create or update a person
+  // It is responsible for:
+  //
+  //  * adding errors messages to the form
+  //  * adding styles to the form fields to show which ones have errors
+  //
+  personWasNotSaved: function (form, errors) {
+    var $form = $(form);
+    var html = JST['templates/errors']({messages: errors.full_messages});
+    $form.find("[data-container=errors]").html(html);
+    $form.find(".field-with-errors").removeClass("field-with-errors");
+    $.each(errors.fields, function () {
+      $form.find("input[name=" + this + "]").closest(".form-row").addClass("field-with-errors");
     });
   }
 
--- spec/features/contact_manager_spec.rb
+++ spec/features/contact_manager_spec.rb
@@ -21,4 +21,27 @@ feature 'The one-page contact manager app' do
     end
   end
 
+  scenario 'User creates a person', js: true do
+    visit '/'
+    fill_in "First Name:", with: "Joe"
+    fill_in "Last Name:", with: "Example"
+    fill_in "Address:", with: "15 Main St"
+    click_button "Create Person"
+
+    expect(page).to have_content("Joe Example")
+    expect(page).to have_content("15 Main St")
+  end
+
+  scenario 'User tries to create a person with incomplete data', js: true do
+    visit '/'
+    click_button "Create Person"
+    expect(page).to have_content("First name can't be blank")
+
+    fill_in "First Name:", with: "Joe"
+    fill_in "Last Name:", with: "Example"
+    click_button "Create Person"
+
+    expect(page).to have_no_content("First name can't be blank")
+  end
+
 end
